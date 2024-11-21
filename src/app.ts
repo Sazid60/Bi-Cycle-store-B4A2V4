8636ed98b764969c61516cb6685ed458116f3167
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2024-11-21 19:59:11 +0600

    Project Initiated

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// Parser
+app.use(express.json());
+app.use(cors());
+
+// application routes
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
